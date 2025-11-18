e901ebffaa3e7eb70a07326210e285318394ac83
vishruth-iyer <137830515+vishruth-iyer@users.noreply.github.com>
2025-11-18 15:31:35 -0800

    Receiver into_stream

--- stream_utils/src/channel/receiver.rs
+++ stream_utils/src/channel/receiver.rs
@@ -7,3 +7,10 @@ impl<T> Receiver<T> for () {
         None
     }
 }
+
+pub fn into_stream<T>(rx: impl Receiver<T>) -> impl futures::Stream<Item = T> {
+    futures::stream::unfold(rx, |mut rx| async {
+        let item = rx.recv().await;
+        item.map(|item| (item, rx))
+    })
+}
