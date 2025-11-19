586c540e7cc97cb7d4ed72b8b65b4ad0fef6c3c9
vishruth-iyer <137830515+vishruth-iyer@users.noreply.github.com>
2025-11-18 18:11:55 -0800

    Separate send returning source info and not returning source info

--- stream_utils/src/download_fanout/download.rs
+++ stream_utils/src/download_fanout/download.rs
@@ -13,6 +13,65 @@ pub struct DownloadFanoutDownload<
     source_info: std::marker::PhantomData<SourceInfo>,
 }
 
+async fn send_helper<Source, Consumers, BroadcasterChannel, EgressSender, Error>(
+    download_fanout: &mut super::DownloadFanout<Source, Consumers>,
+    broadcaster_channel: BroadcasterChannel,
+    egress_tx: EgressSender,
+) -> Result<Consumers::Output, super::error::DownloadFanoutError<Error>>
+where
+    Source: super::source::FanoutSource,
+    Consumers: super::consumer::FanoutConsumerGroup,
+    BroadcasterChannel: channel::Channel<bytes::Bytes>,
+    EgressSender: super::egress::Sender,
+    super::error::DownloadFanoutError<Error>: From<Source::Error> + From<Consumers::Error>,
+{
+    match download_fanout
+        .download_inner::<BroadcasterChannel, EgressSender, Error>(
+            broadcaster_channel,
+            &egress_tx,
+        )
+        .await
+    {
+        Ok(download_fanout_output) => {
+            drop(egress_tx);
+            Ok(download_fanout_output)
+        }
+        Err(e) => {
+            // notify egress receiver that an error occurred
+            // for the egress multipart upload use case, this aborts the upload
+            let _ = egress_tx.send(super::egress::GenericError.into()).await;
+            drop(egress_tx);
+            Err(e)
+        }
+    }
+}
+
+impl<Source, Consumers, BroadcasterChannel, EgressSender>
+    DownloadFanoutDownload<Source, Consumers, BroadcasterChannel, EgressSender, ()>
+where
+    Source: super::source::FanoutSource,
+    Consumers: super::consumer::FanoutConsumerGroup,
+    BroadcasterChannel: channel::Channel<bytes::Bytes>,
+    EgressSender: super::egress::Sender,
+{
+    pub async fn send<Error>(
+        mut self,
+    ) -> Result<Consumers::Output, (Option<super::DownloadFanout<Source, Consumers>>, Error)>
+    where
+        super::error::DownloadFanoutError<Error>: From<Source::Error> + From<Consumers::Error>,
+    {
+        match send_helper(&mut self.download_fanout, self.broadcaster_channel, self.egress_tx).await {
+            Ok(download_fanout_output) => {
+                Ok(download_fanout_output)
+            }
+            Err(e) => {
+                let retry_fanout = e.get_retry_fanout(self.download_fanout);
+                Err((retry_fanout, e.into_inner()))
+            }
+        }
+    }
+}
+
 impl<Source, Consumers, BroadcasterChannel, EgressSender, SourceInfo>
     DownloadFanoutDownload<Source, Consumers, BroadcasterChannel, EgressSender, SourceInfo>
 where
@@ -21,32 +80,17 @@ where
     BroadcasterChannel: channel::Channel<bytes::Bytes>,
     EgressSender: super::egress::Sender,
 {
-    pub async fn send<Error>(
+    pub async fn send_returning_source_info<Error>(
         mut self,
     ) -> Result<(SourceInfo, Consumers::Output), (Option<super::DownloadFanout<Source, Consumers>>, Error)>
     where
         super::error::DownloadFanoutError<Error>: From<Source::Error> + From<Consumers::Error>,
     {
-        match self
-            .download_fanout
-            .download_inner::<BroadcasterChannel, EgressSender, Error>(
-                self.broadcaster_channel,
-                &self.egress_tx,
-            )
-            .await
-        {
+        match send_helper(&mut self.download_fanout, self.broadcaster_channel, self.egress_tx).await {
             Ok(download_fanout_output) => {
-                drop(self.egress_tx);
-                Ok((
-                    self.download_fanout.source.into_info(),
-                    download_fanout_output,
-                ))
+                Ok((self.download_fanout.source.into_info(), download_fanout_output))
             }
             Err(e) => {
-                // notify egress receiver that an error occurred
-                // for the egress multipart upload use case, this aborts the upload
-                let _ = self.egress_tx.send(super::egress::GenericError.into()).await;
-                drop(self.egress_tx);
                 let retry_fanout = e.get_retry_fanout(self.download_fanout);
                 Err((retry_fanout, e.into_inner()))
             }
