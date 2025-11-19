ee77d1cfb56b7f5eac4f87d4db87a12b55e8bd9a
vishruth-iyer <137830515+vishruth-iyer@users.noreply.github.com>
2025-11-19 15:58:24 -0800

    Don't subscribe for no egress

--- stream_utils/src/channel/sender.rs
+++ stream_utils/src/channel/sender.rs
@@ -40,6 +40,21 @@ impl<T> Sender<T> for () {
     async fn send(&self, _item: T) -> Result {
         Result::Failure
     }
+
+    fn _send_from_broadcaster<'a, 'b, U, BroadcasterChannel>(
+        &'a self,
+        broadcaster: &'b mut broadcaster::Broadcaster<U, BroadcasterChannel>,
+    ) -> (
+        &'b mut broadcaster::Broadcaster<U, BroadcasterChannel>,
+        impl Future<Output = ()> + 'a,
+    )
+    where
+        BroadcasterChannel: super::Channel<U>,
+        T: From<U>,
+        BroadcasterChannel::Receiver: 'static,
+    {
+        (broadcaster, futures::future::ready(()))
+    }
 }
 
 pub enum Result {
